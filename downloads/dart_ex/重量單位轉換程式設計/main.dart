256d8148a24d0607f32d0805512995da2746e970
40823102 <40823102@gm.nfu.edu.tw>
2020-01-02 10:15:41 +0800

    加入重量單位轉換程式設計

---
+++ downloads/dart_ex/重量單位轉換程式設計/main.dart
@@ -0,0 +1,16 @@
+int x = 0;
+num ib = 1;
+num kg = 0.45;
+
+void main(){
+  if (x == 0) {
+  num ib2 = kg/0.45;
+			print('ib=$ib2');
+  }
+  else{
+    
+    num kg2 = ib*0.45;
+			print('kg=$kg2');
+  }
+  print('x=0,1切換(ib、kg)');
+  }
